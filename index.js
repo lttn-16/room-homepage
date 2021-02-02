39baecb62519c2348c2153cc3f152ca8ee6d3a41
Thanh Nhi <67329204+lttn-16@users.noreply.github.com>
2021-02-02 10:19:12 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+$('.caroseul-button__left').on('click', function(e) {
+    e.preventDefault();
+    $('.carousel').carousel('prev');
+});
+
+$('.caroseul-button__right').on('click', function(e) {
+    e.preventDefault();
+    $('.carousel').carousel('next');
+});
+
+var src1="./images/icon-hamburger.svg";
+var src2="./images/icon-close.svg";
+
+
+$('#mobile-nav-icon').click(
+    function(){
+
+        $('.navbar-list').toggleClass("showing");
+
+        $('.navbar-list').toggleClass("background");
+        
+        $('.navbar-list').toggleClass("selected");
+
+        var src = $('#mobile-nav-icon').attr('src'); 
+        if(src == src1){
+            $('#mobile-nav-icon').attr('src',src2);
+        }
+        else{
+            $('#mobile-nav-icon').attr('src',src1);
+        }
+    }
+);
